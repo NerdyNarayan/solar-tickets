ea85396339db2cc5d1b18b1940613f571fd71c83
NerdyNarayan <nerdynarayan@gmail.com>
2024-08-24 21:00:26 +0530

    feat: setting up webhook

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,26 @@
+import type { WebhookEvent } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import { db } from '~/server/db'
+import { User } from '../../../../server/db/schema'
+export async function POST(request: Request) {
+  const payload: WebhookEvent = await request.json()
+  if (payload.type === 'user.created') {
+    const { id, first_name, last_name, image_url, username } = payload.data
+    try {
+      await db.insert(User).values({
+        id: id,
+        name: `${first_name || username} ${last_name || ''} `,
+        image: image_url,
+      })
+      return NextResponse.json({ status: 'success' })
+    } catch (e) {
+      console.error(e)
+      return NextResponse.json(
+        { status: 'error', message: 'Failed to create user' },
+        { status: 500 },
+      )
+    }
+  } else {
+    return NextResponse.json({ status: 'unsupported' })
+  }
+}
